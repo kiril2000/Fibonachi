28a5724f33161e39b5345e893feea41d54191cbb
Kiril Chobanov <62597135+kiril2000@users.noreply.github.com>
2020-04-05 16:07:56 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstN = 0, secondN = 1, thirdN = 0;
+
+            Console.Write("Enter number: ");
+            int lenght = Int32.Parse(Console.ReadLine());
+
+            Console.Write("0, 1,");
+
+            for (int i = 2; i < lenght; i++)
+            {
+                thirdN = firstN + secondN; 
+                Console.Write(" {0},", thirdN);
+                firstN = secondN;
+                secondN = thirdN;
+               
+            }
+        }
+    }
+}
